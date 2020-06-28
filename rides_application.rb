86cf365bea384953d0cd3a8235e80cc6bddeba79
Pavel Egorov <PavelEgorov@MrFSP.local>
2020-06-28 17:53:23 +0300

    rides_application.rb refactored

--- rides_application.rb
+++ rides_application.rb
@@ -1,37 +1,50 @@
 require 'json'
 
 class RidesApplication
-  def call(env)
-    request = Rack::Request.new(env)
-    response = Rack::Response.new
-    response.headers['Content-Type'] = 'application/json'
+  include ApplicationHelpers
 
-    if env['PATH_INFO'] == ''
-      if request.post?
-        begin
-          ride = JSON.parse(request.body.read)
-          if ride['user_id'].nil?
-            response.status = 400
-            response.write('user_id field is required')
-          else
-            Database.add_ride(ride)
-            response.write(JSON.generate({ message: 'Ride received' }))
-          end
-        rescue JSON::ParserError
-          response.status = 400
-          response.write('Invalid JSON')
-        end
-      else
-        response.write(JSON.generate(Database.rides))
-      end
-    elsif env['PATH_INFO'] =~ %r{/\d+}
-      id = env['PATH_INFO'].split('/').last.to_i
-      response.write(JSON.generate(Database.rides[id]))
+  def call(env) 
+    request  = Rack::Request.new(env)
+    response = Rack::Response.new
+    response.headers["Content-Type"] = "application/json"
+
+    case request.path_info
+    when request.post? && ""
+      post_a_ride(request, response)
+    when request.get? && ""
+      get_all_rides(request, response)
+    when %r{/\d+}
+      get_a_ride(request, response)
     else
-      response.status = 404
-      response.write('Nothing Here')
+      missing(response)
     end
 
     response.finish
+  end
+
+  def post_a_ride(request, response)
+    ride = JSON.parse(request.body.read)
+    if ride["user_id"].nil?
+      error(response, "user_id field is required")
+    else
+      Database.add_ride(ride)
+      respond_with_object(response, { message: "Ride received"})
+    end
+  rescue JSON::ParserError
+    error(response, "Invalid JSON")
+  end
+
+  def get_all_rides(request, response)
+    respond_with_object(response, Database.rides)
+  end
+
+  def get_a_ride(request, response)
+    id = request.path_info.split("/").last.to_i
+    ride = Database.rides[id]
+    if ride.nil?
+      error(response, "No ride with id #{id}", 404)
+    else
+      respond_with_object(response, ride)
+    end
   end
 end
