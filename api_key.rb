1c757e64ead349736dbd2d388d404d66c729425b
Pavel Egorov <PavelEgorov@MrFSP.local>
2020-06-29 08:16:22 +0300

    api_key.rb updated

--- api_key.rb
+++ api_key.rb
@@ -1,9 +1,18 @@
-class api_key
-  def initialize(app, opts = {})
+class ApiKey
+  def initialize(app, opts={})
     @app = app
+    @database = opts[:database]
   end
 
   def call(env)
-    @app.call(env)
+    api_key = env['HTTP_API_KEY']
+    user = @database.user_by_api_key(env["HTTP_API_KEY"])
+
+    if user.nil?
+      [403, {}, ["Forbidden"]]
+    else
+      env["rides_app.user_id"] = user[:id]
+      @app.call(env)
+    end
   end
 end
