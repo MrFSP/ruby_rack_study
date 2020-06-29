d54aceefc344310ff7216da87680fe7343ca0af7
Pavel Egorov <PavelEgorov@MrFSP.local>
2020-06-29 10:10:13 +0300

    env_application.rb refactored

--- env_application.rb
+++ env_application.rb
@@ -2,7 +2,8 @@ class EnvApplication
   def call(env)
     if env['PATH_INFO'] == ''
       request = Rack::Request.new(env)
-      [200, {}, [request.env.to_s]]
+      env_print = request.env.reduce('') { |acc, item| "#{acc}\n#{item.to_s}" }
+      [200, {}, [env_print]]
     else
       [404, {}, ['<h1>Nothing Here!</h1>']]
     end
